fa69da401f92aa2780f6c2b5157d3126c617a19d
Daniil Lyubanko <75521025+nihongo-deb@users.noreply.github.com>
2023-08-26 20:27:18 +0700

    create project

---
+++ src/main/java/org/nihongo_deb/DeadLinkSearcher/DeadLinkSearcherApplication.java
@@ -0,0 +1,13 @@
+package org.nihongo_deb.DeadLinkSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadLinkSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadLinkSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nihongo_deb/DeadLinkSearcher/DeadLinkSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nihongo_deb.DeadLinkSearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadLinkSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
