b6e4669f5d49256f6a1a6464c78eaae39dd18315
Daniil Lyubanko <75521025+nihongo-deb@users.noreply.github.com>
2023-09-10 16:19:05 +0700

    add Role changes

--- src/main/java/org/nihongo_deb/DeadLinkSearcher/Entity/Role.java
+++ src/main/java/org/nihongo_deb/DeadLinkSearcher/Entity/Role.java
@@ -1,5 +1,17 @@
 package org.nihongo_deb.DeadLinkSearcher.Entity;
 
 public enum Role {
-    ROLE_USER, ROLE_ADMIN
+    ROLE_USER, ROLE_ADMIN;
+
+    public static Role stringToRole(String strRole){
+        switch (strRole){
+            case "ROLE_USER" -> {
+                return ROLE_USER;
+            }
+            case "ROLE_ADMIN" -> {
+                return ROLE_ADMIN;
+            }
+        }
+        return null;
+    }
 }
